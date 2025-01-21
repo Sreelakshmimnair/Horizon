d53022ac22c32cc46c46ff1db26efb3409f420f1
sayanthcv <sayanthanoob@gmail.com>
2025-01-21 13:08:41 +0530

    hello

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),  // Set SplashScreen as the home page
+      home: const SplashScreen(),  
     );
   }
 }
